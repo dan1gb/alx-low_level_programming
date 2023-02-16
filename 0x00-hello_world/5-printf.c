e6b46f6c5abe8f2e7baf4261317f26360a19e555
Danny Kimani <kimwadan@gmail.com>
2023-02-16 08:30:11 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *main function declared
- *Return: 0;
+ *main - this is the main function
+ *Return: 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
- return 0;
+return (0);
 }
