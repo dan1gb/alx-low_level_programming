9c1be7e3db0443d70fdb467e7a8b42483a589b30
Danny Kimani <kimwadan@gmail.com>
2023-02-16 10:54:34 -0800

    output ass error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - is the main function
+ *Return: 1 returns error
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n");
+return (1);
+}
