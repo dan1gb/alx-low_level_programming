ffdc7819dad6e32d12be65b4c687fbbae924d2e6
Danny Kimani <kimwadan@gmail.com>
2023-02-17 01:53:09 -0800

    print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *Return: returns 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
