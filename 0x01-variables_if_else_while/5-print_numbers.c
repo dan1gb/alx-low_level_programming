2c1575b330423dff1ba1839f2f0379a9a684a178
Danny Kimani <kimwadan@gmail.com>
2023-02-17 02:30:44 -0800

    print numbers between 0 and 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- *main - this is the main function
- *Return: return 0
+ * main - this is the main function
+ * return: returns 0
  */
 int main(void)
 {
-int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int i;
 for (i = 0; i < 10; i++)
 {
-printf("%d", numbers[i]);
+printf("%d", i);
 }
-printf("\n");
+putchar('\n');
 return (0);
 }
