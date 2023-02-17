24a1083d194c02a3d543f0d1e2a73079170fc23a
Danny Kimani <kimwadan@gmail.com>
2023-02-17 02:45:48 -0800

    print numberz using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- *main - this is the main function
- *Return: return 0
- */
+ * main function denotes start
+ * Return: returns a zero
+*/
 int main(void)
 {
-int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-int i;
-for (i = 0; i < 10; i++)
-putchar((numbers[i] + 48));
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0');
+}
 putchar('\n');
 return (0);
 }
