9bdc088c053962d83ffc4f72040d39b1f27df06d
Danny Kimani <kimwadan@gmail.com>
2023-02-21 06:57:26 -0800

    check on number line

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int print_sign(int n) - function to print the sign of a numebr
+ * print_sign(int n) - function to print the sign of a numebr
  * Return: returns 1 if greater than 0,0 if zero, -1 if less than zero
  * @n - argument accepted by function
  */
