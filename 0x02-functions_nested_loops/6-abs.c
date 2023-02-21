439559341d7b00733153071dcb0d0e15501d8fee
Danny Kimani <kimwadan@gmail.com>
2023-02-21 10:07:19 -0800

    absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - function to find absolute numbers
+ * @num - argument accepted
+ * Return: returns the number in absolute form
+*/
+int _abs(int num)
+{
+if (num > 0)
+{
+	return (num);
+}
+if (num < 0)
+{
+	return (-num);
+}
+else
+{
+	return (num);
+}
+}
