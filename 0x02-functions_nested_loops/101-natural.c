3240e083495ffd06d6c6e47270602cf3502c39d0
Danny Kimani <kimwadan@gmail.com>
2023-02-21 14:40:35 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - main entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
