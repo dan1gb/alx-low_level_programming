0248a16eb8d7988d182bea4da3b37eabe92d0e55
Danny Kimani <kimwadan@gmail.com>
2023-02-21 15:42:43 -0800

    fibonacci ingine

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,15 +12,15 @@ int main(void)
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-		printf("%lu, ", sum);
+		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
 
 		if (count == 49)
 			printf("\n");
-	/*	else
-			printf(","); */
+	else
+			printf(", "); 
 	}
 	return (0);
 }
