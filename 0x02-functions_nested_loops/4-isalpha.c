235d96face6aa6f5a1e0ab9d94e8312512ec2b31
Danny Kimani <kimwadan@gmail.com>
2023-02-21 06:27:23 -0800

    check if alphabetic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * _isalpha - function to check alphabet
+ * Return: returns 0 if alphabet, else 0
+ * @c: argument accepted to be worked upon
+ */
 int _isalpha(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 	return (1);
 	}
