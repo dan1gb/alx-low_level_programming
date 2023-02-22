accccb8296c8939b92eabac119c233b2a1dc55c2
Danny Kimani <kimwadan@gmail.com>
2023-02-22 00:45:34 -0800

    multiplication kosokoso

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,27 +1,43 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * times_table - function to calculate multiples
- * @void: returns void
- * Return: return 0 if successful
+ * times_table - function to print multiplication grid of 9
+ * @void: no return value
+ * Return: Always 0
+ * 
  */
+
 void times_table(void)
 {
-int row, col;
+	int row, column, product, tens, ones;
 
-for (row = 0; row < 10; row++)
-{
-for (col = 0; col < 10; col++)
-{
-if (col != 9)
-{
-printf("%2d,", row * col);
-}
-else
-{
-printf("%2d", row * col);
-}
-}
-printf("\n");
-}
+	for (row = 0; row < 10; row++)
+	{
+		for (column = 0; column < 10; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
