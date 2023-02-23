814d8248d9bc6423afaf18742e66d28ad01d0aca
Danny Kimani <kimwadan@gmail.com>
2023-02-22 23:30:09 -0800

    Debbing side of life

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+* Return: returns 0 if successful
+*/
+void positive_or_negative(int i)
+{
+/*srand(time(0));
+i = rand() - RAND_MAX / 2;*/
+if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+/*else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+return (0);*/
+}
