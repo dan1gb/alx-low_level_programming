3f2ec9ab94749352b566b21e3ccc00713169a41c
Danny Kimani <kimwadan@gmail.com>
2023-02-23 10:35:44 -0800

    Task 0. Check uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * _isupper - function to check if letter is uppercase
- * @x: To be checked
+ * _isupper(int c) - function to check if letter is uppercase
+ * @c: To be checked
  * Return: 1 for upper letter, 0 otherwise
  */
 
 int _isupper(int c)
 {
-c = 0;
 if (c >= 65 && c <= 90)
 {
-	return (1);
+return (1);
 }
 return (0);
 }
