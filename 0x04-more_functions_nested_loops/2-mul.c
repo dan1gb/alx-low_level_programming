a0ced3f6fe0a7d5b177e282106f2fad9de585730
Danny Kimani <kimwadan@gmail.com>
2023-02-23 11:01:27 -0800

    multiplication of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function to multiply different numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: returns the product
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,5 @@
 int _putchar(char c);
 int _isupper(int x);
 int _isdigit(int c);
-
+int mul(int a, int b);
 #endif
