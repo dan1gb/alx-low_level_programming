40c331dde159dfbdb63890c9508f2438e446b69b
Danny Kimani <kimwadan@gmail.com>
2023-02-23 12:21:16 -0800

    num 1-14, 10x

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ */
+void more_numbers(void)
+{
+	int count, num;
+	for (count = 0; count <= 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+	_putchar((num / 10) + '0');
+		}
+		_putchar((num % 10) + '0');
+	}
+		_putchar('\n');
+}
+}
