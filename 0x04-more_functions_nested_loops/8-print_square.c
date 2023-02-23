93b51d08c4700d3991f0479872f03aa24153873f
Danny Kimani <kimwadan@gmail.com>
2023-02-23 14:09:04 -0800

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_square(int size)
+{
+	int row, col;
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+for (col = 0; col < size; col++)
+{
+	_putchar(35);
+}
+_putchar('\n');
+}}
+}
