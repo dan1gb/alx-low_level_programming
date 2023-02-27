d1e3e7dae858158e9dda1fc77d3759ec53149eaa
Danny Kimani <kimwadan@gmail.com>
2023-02-27 11:09:20 -0800

    reverse 2nd try

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - function name
+ * @s: pointer parameter
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
