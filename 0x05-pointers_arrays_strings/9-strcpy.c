6d39b4e230dec2069a4a9aa71400d6459a652b8b
Danny Kimani <kimwadan@gmail.com>
2023-03-01 15:00:42 -0800

    copy from source to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,21 @@
 #include "main.h"
 /**
- * *_strcopy - function to copy src to dest
- * @*dest: parameter 1
- * @*src: source
- * return: destination
+ * *_strcpy - function to copy src to dest
+ * @dest: parameter 1
+ * @src: source
+ * Return: returns the destination
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	for(i = 0 ; src[i] != '\0' ; i++)
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	else
+	for (i = 0 ; src[i] != '\0' ; i++)
 	{
 		dest[i] = src[i];
 	}
