33cdd2c6c7c92eeeda606115168ce7e2dcf5927d
Danny Kimani <kimwadan@gmail.com>
2023-03-03 11:49:43 -0800

    concatenate v2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * @strncat - function to concatenate strings
+ * @dest: destination character
+ * @src: source character
+ * @n number of array fields
+ * Return: returns destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int ldest = 0, lsrc = 0, i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		ldest++;
+	for (i = 0; src[i] != '\0'; i++)
+		lsrc++;
+	for (i = 0; i < n; i++)
+		dest[ldest + 1] = src[i];
+	return (dest);
+}
