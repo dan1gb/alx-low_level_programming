3814753f7d75327deb0bc039a08b99d0036ac06f
Danny Kimani <kimwadan@gmail.com>
2023-03-03 12:41:23 -0800

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
+int lsrc = 0;
+while (src[i++])
+{
+	lsrc++;
+}
+for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (i = 0; i < n; i++)
+	for (i = lsrc; i < n; i++)
 	{
 	dest[i] = '\0';
 }
