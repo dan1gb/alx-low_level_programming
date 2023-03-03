3145448e304ee53924a4f2c46651283ec195670c
Danny Kimani <kimwadan@gmail.com>
2023-03-03 13:54:31 -0800

    convert string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,11 +8,14 @@ char *string_toupper(char *str)
 {
 	int index = 0;
 
-	while(str[index])
+	while (str[index])
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] = str[index] - 32;
+			str[index] -= 32;
+
 		index++;
 	}
+
 	return (str);
 }
+
