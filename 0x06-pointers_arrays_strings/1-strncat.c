e4c5729f8d0918d70d26fa2346cda79855680054
Danny Kimani <kimwadan@gmail.com>
2023-03-03 13:56:34 -0800

    concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,19 +7,12 @@
  * Return: returns destination string
  */
 char *_strncat(char *dest, char *src, int n)
+
 {
-	int ldest = 0, i = 0;
-
-	while (dest[ldest] != '\0')
-	{
-		ldest++;
-	}
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-	dest[ldest + 1] = src[i];
-	}
-
-	/* Add null terminator to end of dest */
-	dest[ldest + n] = '\0';
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
