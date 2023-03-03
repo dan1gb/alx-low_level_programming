70ee272adc46216ea6b98430ad275a62af2628ef
Danny Kimani <kimwadan@gmail.com>
2023-03-03 13:13:01 -0800

    diff between pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - function to compare strings
+ * @s1: string 1
+ * @s2: string 2
+ * return: return difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	char diff = *s1 - *s2;
+while((*s1 && s2) && (*s1 == *s2))
+{
+	s1++;
+	s2++;
+}
+return (diff);
+}
