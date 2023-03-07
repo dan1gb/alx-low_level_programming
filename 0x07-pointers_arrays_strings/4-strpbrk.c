98e23f47462d2cb272b47f83f57b1d5fd39b6381
Danny Kimani <kimwadan@gmail.com>
2023-03-07 01:41:33 -0800

    Strip bark

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _strpbrk - function to strip bark
+ * @s: argument 1
+ * @accept: argument 2
+ * Return: Always 0
+ */
 char *_strpbrk(char *s, char *accept)
 {
 	int i = 0;
