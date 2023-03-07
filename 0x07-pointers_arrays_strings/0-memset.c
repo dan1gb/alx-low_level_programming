1eed4bf211fa553f89b88eedada9bb0fd5377f59
Danny Kimani <kimwadan@gmail.com>
2023-03-07 08:25:02 -0800

    Memory copy and memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - check main
+ * @s: arg1
+ * @b: arg2
+ * @n: arg 3
+ * Return: Always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
