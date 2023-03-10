48fb9580e7ab394618bb0773dadccb678caa62da
Danny Kimani <kimwadan@gmail.com>
2023-03-10 11:46:08 -0800

    Saitan of a task. Mnadhani traversing searching for 20 files ni mcheso?

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -9,19 +9,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+int i = 0;
 int lsrc = 0;
 while (src[i++])
 {
-	lsrc++;
+lsrc++;
 }
 for (i = 0; i < n && src[i]; i++)
-	{
-		dest[i] = src[i];
-	}
-	for (i = lsrc; i < n; i++)
-	{
-	dest[i] = '\0';
+{
+dest[i] = src[i];
+}
+for (i = lsrc; i < n; i++)
+{
+dest[i] = '\0';
 }
 return (dest);
 }
