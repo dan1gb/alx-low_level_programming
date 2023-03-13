a1fd445b6c259d99f6a66c6f05fe2b38f3d4a4ee
Danny Kimani <kimwadan@gmail.com>
2023-03-13 02:33:48 -0700

    task 1 print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+* main - program entry point
+* @argc: argument counter
+* @argv: argument vector
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
